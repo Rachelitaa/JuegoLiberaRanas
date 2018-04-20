3d84b033c92845868ead2ed9ccce1279a942b752
Rachelitaa <47877604e@iespoblenou.org>
2018-04-20 10:52:11 +0200

    arreglo la animación y añado el animator
    arreglo la animación y añado el animator

--- LiberandoRanas/Assets/move.cs
+++ LiberandoRanas/Assets/move.cs
@@ -3,34 +3,15 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class move : MonoBehaviour {
-    public float speed = 2.0f;
-    public float alturaSalto = 5f;
-    public bool grounded = true;
 
-
-    private void FixedUpdate() {
-
-        //Para movernos en el eje x
-        //Get Input from Arrow Keys,AD,Gamepads
+    public float speed = 5.0f;
+    private void FixedUpdate()
+    {
         float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
 
-        Vector2 dir = new Vector2(h, 0);
-        GetComponent<Rigidbody2D>().velocity += dir.normalized * speed;
-
-        //Control de que estemos tocando el suelo
-        if (transform.position.y < -5.74f)
-        {
-            grounded = true;
-        }
-
-        //Tecla Fecha arriba para saltar
-        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
-        {
-            GetComponent<Rigidbody2D>().velocity += Vector2.up * alturaSalto;
-            grounded = false;
-
-
-        }
+        Vector2 dir = new Vector2(h, v);
+        GetComponent<Rigidbody2D>().velocity = dir.normalized * speed;
+        GetComponent<Animator>().SetBool("saltando", (v > 0));
     }
-	
 }

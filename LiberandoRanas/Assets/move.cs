b2aa01fc573b7ae5a98858db7ced19bdf2e1420d
Rachelitaa <47877604e@iespoblenou.org>
2018-04-20 08:31:26 +0200

    El salto de la rana
    programo el salto de la rana.

---
+++ LiberandoRanas/Assets/move.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour {
+    public float speed = 2.0f;
+    public float alturaSalto = 5f;
+
+    
+    private void FixedUpdate() {
+
+        //Para movernos en el eje x
+        //Get Input from Arrow Keys,AD,Gamepads
+        float h = Input.GetAxisRaw("Horizontal");
+
+        Vector2 dir = new Vector2(h, 0);
+        GetComponent<Rigidbody2D>().velocity += dir.normalized * speed;
+
+        //Tecla Fecha arriba para saltar
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            GetComponent<Rigidbody2D>().velocity += Vector2.up * alturaSalto;
+            
+        }
+    }
+	
+}

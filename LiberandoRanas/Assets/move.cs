ec55f65175e8bd28911baabfcd52a820046b5c00
Rachelitaa <47877604e@iespoblenou.org>
2018-04-20 08:44:14 +0200

    añado condición grounded
    añado condición grounded para controlar que no me deje volver a saltar mientras estoy en el aire

--- LiberandoRanas/Assets/move.cs
+++ LiberandoRanas/Assets/move.cs
@@ -5,8 +5,9 @@ using UnityEngine;
 public class move : MonoBehaviour {
     public float speed = 2.0f;
     public float alturaSalto = 5f;
+    public bool grounded = true;
 
-    
+
     private void FixedUpdate() {
 
         //Para movernos en el eje x
@@ -16,11 +17,19 @@ public class move : MonoBehaviour {
         Vector2 dir = new Vector2(h, 0);
         GetComponent<Rigidbody2D>().velocity += dir.normalized * speed;
 
+        //Control de que estemos tocando el suelo
+        if (transform.position.y < -5.74f)
+        {
+            grounded = true;
+        }
+
         //Tecla Fecha arriba para saltar
-        if (Input.GetKeyDown(KeyCode.UpArrow))
+        if (Input.GetKeyDown(KeyCode.UpArrow) && grounded)
         {
             GetComponent<Rigidbody2D>().velocity += Vector2.up * alturaSalto;
-            
+            grounded = false;
+
+
         }
     }
 	
